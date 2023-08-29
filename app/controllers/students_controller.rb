ef8edec31208a298a70747b383479da0e3936c20
rebekahfigueroa <figueroarebekah@gmail.com>
2023-08-29 11:59:06 -0500

    commit tree 26cbadc918fdafdc69226112b8d2661d78ae43f0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # students = params[:name] ? Student.find(:all, :name=params[:name]) : Student.all 
+    students = params[:name] ? Student.where("first_name like ? OR last_name like ?", params[:name], params[:name]) : Student.all 
     render json: students
   end
+
+  def info
+    student = Student.find(params[:id])
+    render json: student 
+  end 
 
 end
